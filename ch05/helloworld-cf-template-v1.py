993272c922c746dd948243e7710f1a71cbfd09e9
Nathaniel Felsen <n@medium.com>
2016-10-30 19:15:55 -0700

    cleanup

--- ch05/helloworld-cf-template-v1.py
+++ ch05/helloworld-cf-template-v1.py
@@ -1,35 +1,46 @@
 #!/usr/bin/env python
 
-from troposphere import Ref, Template, ec2, Parameter, Output, Join, GetAtt, Base64, cloudformation
+from troposphere import (
+    Base64,
+    cloudformation,
+    ec2,
+    GetAtt,
+    Join,
+    Output,
+    Parameter,
+    Ref,
+    Template,
+)
 
 ApplicationPort = "3000"
 
 t = Template()
-kp = Parameter(
+
+kp = t.add_parameter(Parameter(
     "KeyPair",
     Description="Name of an existing EC2 KeyPair to SSH",
     Type="AWS::EC2::KeyPair::KeyName",
-    ConstraintDescription="must be the name of an existing EC2 KeyPair."
-  )
-t.add_parameter(kp)
+    ConstraintDescription="must be the name of an existing EC2 KeyPair.",
+))
 
-sg = ec2.SecurityGroup('SecurityGroup')
-sg.GroupDescription = "Allow SSH and TCP/%s access" \
-  % ApplicationPort
-sg.SecurityGroupIngress = [
-ec2.SecurityGroupRule(
-    IpProtocol="tcp",
-    FromPort="22",
-    ToPort="22",
-    CidrIp="0.0.0.0/0",
-  ),
-ec2.SecurityGroupRule(
-    IpProtocol="tcp",
-    FromPort=ApplicationPort,
-    ToPort=ApplicationPort,
-    CidrIp="0.0.0.0/0",
-  )]
-t.add_resource(sg)
+sg = t.add_resource(ec2.SecurityGroup(
+    "SecurityGroup",
+    GroupDescription="Allow SSH and TCP/{} access".format(ApplicationPort),
+    SecurityGroupIngress=[
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="22",
+            ToPort="22",
+            CidrIp="0.0.0.0/0",
+        ),
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort=ApplicationPort,
+            ToPort=ApplicationPort,
+            CidrIp="0.0.0.0/0",
+          ),
+    ],
+))
 
 ud = Base64(Join('\n', [
         "#!/bin/bash",
@@ -38,30 +49,31 @@ ud = Base64(Join('\n', [
         "wget http://bit.ly/1QRjORH -O /home/ec2-user/helloworld.js",
         "wget http://bit.ly/1Q4QBhB -O /etc/init/helloworld.conf",
         "start helloworld"
-    ]))
+    ],
+))
 
-instance = ec2.Instance("instance",
+instance = t.add_resource(ec2.Instance(
+    "instance",
     ImageId="ami-f5f41398",
     InstanceType="t2.micro",
     SecurityGroups=[Ref(sg)],
     KeyName=Ref(kp),
-    UserData=ud
-)
-t.add_resource(instance)
+    UserData=ud,
+))
 
-ip = Output(
+t.add_output(Output(
     "InstancePublicIp",
     Description="Public IP of our instance.",
-    Value=GetAtt(instance, "PublicIp")
-  )
-t.add_output(ip)
+    Value=GetAtt(instance, "PublicIp"),
+))
 
-web = Output(
+t.add_output(Output(
     "WebUrl",
     Description="Application endpoint",
-    Value=Join("", ["http://", GetAtt(instance, "PublicDnsName"), \
-      ":", ApplicationPort])
-  )
-t.add_output(web) 
+    Value=Join("", [
+        "http://", GetAtt(instance, "PublicDnsName"),
+        ":", ApplicationPort
+    ]),
+))
 
 print t.to_json()
